5c78f92e41777912e1683cc98d7d9a38727d0d32
ytwdyfweguh <js1pcdematt@hotmail.com>
2024-05-06 20:24:52 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const uniqueElements = (arr) => [...new Set(arr)];
+function getHint(secret, guess) {
+  let bulls = 0;
+  let cows = 0;
+  const map = new Map();
+  for (let i = 0; i < secret.length; i++) {
+    if (secret[i] === guess[i]) {
+      bulls++;
+    } else {
+      map.set(secret[i], (map.get(secret[i]) || 0) + 1);
+    }
+  }
+  for (let i = 0; i < guess.length; i++) {
+    if (secret[i] !== guess[i] && map.has(guess[i]) && map.get(guess[i]) > 0) {
+      cows++;
+      map.set(guess[i], map.get(guess[i]) - 1);
+    }
+  }
+  return `${bulls}A${cows}B`;
+}
