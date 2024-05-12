5927c5f4107d6bed9bfaf211a4f9256fb660c76c
ytwdyfweguh <js1pcdematt@hotmail.com>
2024-05-12 09:43:14 +0700

    update task

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function rightSideView(root) {
-  if (!root) return [];
-  const result = [];
-  let level = 0;
-  const traverse = (node, level) => {
-    if (!node) return;
-    if (result[level] === undefined) result[level] = node.val;
-    traverse(node.right, level + 1);
-    traverse(node.left, level + 1);
-  };
-  traverse(root, level);
-  return result;
-}
+console.log("world");
