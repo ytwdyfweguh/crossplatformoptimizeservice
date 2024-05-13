3639ea1f380e973cf215b9bcbc525524515628df
ytwdyfweguh <js1pcdematt@hotmail.com>
2024-05-13 11:03:48 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("world");
+function findPeakElement(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] < nums[mid + 1]) left = mid + 1;
+    else right = mid;
+  }
+  return left;
+}
