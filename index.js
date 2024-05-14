28bdf6ea8378829454a4fe61397a29c683da2e50
ytwdyfweguh <js1pcdematt@hotmail.com>
2024-05-15 04:00:12 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-function findPeakElement(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] < nums[mid + 1]) left = mid + 1;
-    else right = mid;
-  }
-  return left;
-}
+const deepFlatten = (arr) =>
+  [].concat(...arr.map((v) => (Array.isArray(v) ? deepFlatten(v) : v)));
